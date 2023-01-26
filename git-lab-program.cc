16e9436193686d152fc28354e39dfe6d5eac7ce3
Mason Gill <mg197119@ohio.edu>
2023-01-25 19:22:47 -0500

    Lab Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Mason Gill
+ *        Date: 01/25/2001
+ * Description: Git Hub Lab Two
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+   cout << "Hello Git" << endl;
+    return 0;
+}// main
